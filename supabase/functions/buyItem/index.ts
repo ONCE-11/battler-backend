643c476abb85a8e29464534de605f6df925b978c
Orlando Caraballo <orlandocaraballo@gmail.com>
2024-02-16 00:09:38 -0500

    Moved to using Promise.all for buyItem function

--- supabase/functions/buyItem/index.ts
+++ supabase/functions/buyItem/index.ts
@@ -23,54 +23,50 @@ Deno.serve(async (req) => {
     );
 
     // load our most current data
-    const { data: itemsData, error: itemsError } = await supabase
-      .from("items")
-      .select("price")
-      .eq("id", itemId)
-      .single();
+    const [
+      { data: itemsData, error: itemsError },
+      { data: profileData, error: profileError },
+    ] = await Promise.all([
+      supabase.from("items").select("price").eq("id", itemId).single(),
+      supabase.from("profiles").select().eq("user_id", userId).single(),
+    ]);
+
+    console.log({
+      itemsData,
+      itemsError,
+      profileData,
+      profileError,
+    });
 
     if (itemsError) throw itemsError;
-
-    const { data: profileData, error: profileError } = await supabase
-      .from("profiles")
-      .select()
-      .eq("user_id", userId)
-      .single();
-
     if (profileError) throw profileError;
 
     // check to see if character has enough pesos
-    const remainingPesos = profileData.pesos - itemsData.price;
+    const remainingPesos = profileData!.pesos - itemsData!.price;
 
-    if (remainingPesos >= 0) {
-      remainingPesos;
+    if (remainingPesos < 0)
+      throw Error(`Character ${characterId} does not have enough pesos`);
 
-      const { error } = await supabase
-        .from("profiles")
-        .update({
-          pesos: remainingPesos,
-        })
-        .eq("user_id", userId);
-
-      if (error) throw error;
-    } else {
-      throw Error("Character does not have enough pesos");
-    }
-
-    const { error: inventoryUpdateError } = await supabase
-      .from("inventories")
-      .insert([
-        {
-          item_id: itemId,
-          character_id: characterId,
-        },
+    const [{ error: profileUpdateError }, { error: inventoryUpdateError }] =
+      await Promise.all([
+        supabase
+          .from("profiles")
+          .update({
+            pesos: remainingPesos,
+          })
+          .eq("user_id", userId),
+        supabase.from("inventories").insert([
+          {
+            item_id: itemId,
+            character_id: characterId,
+          },
+        ]),
       ]);
 
+    if (profileUpdateError) throw profileUpdateError;
     if (inventoryUpdateError) throw inventoryUpdateError;
 
-    const data = { remainingPesos };
-
-    return new Response(JSON.stringify(data), {
+    return new Response(JSON.stringify({ remainingPesos }), {
       headers: { ...corsHeaders, "Content-Type": "application/json" },
       status: 200,
     });
